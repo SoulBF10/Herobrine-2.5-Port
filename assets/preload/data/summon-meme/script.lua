fe55c75e5cf72efafbfbe298735ceb08f1e5cafb
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2022-10-01 10:43:26 -0300

    Add files via upload

---
+++ assets/preload/data/summon-meme/script.lua
@@ -0,0 +1,48 @@
+
+
+function onStepHit()
+
+    if curStep == 1 then
+        triggerEvent("Chat", "<Server> We didn't have enough money to make another song because we are poor, so here you go", "true,false")
+    end
+    if curStep == 16 then
+        triggerEvent("Chat", "<Server> sorry jdst for making you do this", "true,false")
+    end
+    if curStep == 1862 then
+        triggerEvent("Chat", "<Aaron> Man we waited two days for this shit", "false,true")
+    end
+    if curStep == 1865 then
+        triggerEvent("Chat", "<Magi> Fuck this", "false,true")
+    end
+    if curStep == 1868 then
+        triggerEvent("Chat", "<Lemmeo> 2 DAYS FOR THIS AJHUFADSIUHFHKIUDSFHUIAK", "false,true")
+    end
+    if curStep == 1871 then
+        triggerEvent("Chat", "<Z11> i hate life", "false,true")
+    end
+    if curStep == 1874 then
+        triggerEvent("Chat", "<Bonk> the fuck", "false,true")
+    end
+    if curStep == 1877 then
+        triggerEvent("Chat", "<JDST> im going back to sleep, i regret making this", "false,true")
+    end
+    if curStep == 1874 then
+        triggerEvent("Chat", "<Bl1tz> SDHJFIHAJSDFAHJKDSFHJKDSFJHKADFSHJK", "false,true")
+    end
+    if curStep == 1877 then
+        triggerEvent("Chat", "Aaron has left the game", "true,false")
+        triggerEvent("Chat", "Magi has left the game", "true,false")
+        triggerEvent("Chat", "Lemmeo has left the game", "true,false")
+        triggerEvent("Chat", "Z11 has left the game", "true,false")
+        triggerEvent("Chat", "Bonk has left the game", "true,false")
+        triggerEvent("Chat", "JDST has left the game", "true,false")
+        triggerEvent("Chat", "Bl1tz has left the game", "true,false")
+        --sendMessage("Aaron has left the game", true, false)
+        --sendMessage("Magi has left the game", true, false)
+        --sendMessage("Lemmeo has left the game", true, false)
+        --sendMessage("Z11 has left the game", true, false)
+        --sendMessage("Bonk has left the game", true, false)
+        --sendMessage("JDST has left the game", true, false)
+        --sendMessage("Bl1tz has left the game", true, false)
+    end
+end

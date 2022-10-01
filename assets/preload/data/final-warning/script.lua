2c21599637a00ab2896a1878e53b356f91757fac
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2022-10-01 09:19:27 -0300

    Add files via upload

---
+++ assets/preload/data/final-warning/script.lua
@@ -0,0 +1,23 @@
+function onBeatHit()
+
+    if curBeat == 40 then
+        triggerEvent("Chat", "<Magi> btw hit the fire notes!", "false,false")
+        --sendMessage("<Magi> btw hit the fire notes!", false, false)
+    end
+    if curBeat == 44 then
+        --sendMessage("<Magi> AND DONT FORGET TO HIT THEM!!!!!!!", false, true)
+        triggerEvent("Chat", "<Magi> AND DONT FORGET TO HIT THEM!!!!!!!", "false,true")
+    end
+    if curBeat == 48 then
+        triggerEvent("Chat", "<Magi> thanks :)", "false,false")
+        --sendMessage("<Magi> thanks :)", false, false)
+    end
+    if curBeat == 137 then
+        triggerEvent("Chat", "<Herobrine> Think fast, Boyfriend!", "false,false")
+        --sendMessage("<Herobrine> Think fast {USERNAME}!", false, false)
+    end
+    if curBeat == 138 then
+        triggerEvent("Chat", "<Herobrine> Hit these!", "false,false")
+        --sendMessage("<Herobrine> Hit these!", false, false)
+    end
+end

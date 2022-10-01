172fa92172516d6a885632359c2e3d82d4f7b354
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2022-10-01 10:32:36 -0300

    Add files via upload

---
+++ assets/preload/data/shootout/script.lua
@@ -0,0 +1,38 @@
+function onStepHit()
+    if curStep == 80 then
+        shootthem(0.5)
+    end
+    if curStep == 128 then
+        shootthem(0.5)
+    end
+    if curStep == 256 then
+        shootthem(0.5)
+    end
+    if curStep == 512 then
+        shootthem(0.5)
+    end
+    if curStep == 663 then
+        shootthem(0.5)
+    end
+    if curStep == 672 then
+        shootthem(0.5)
+    end
+    if curStep == 926 then
+        shootthem(0.5)
+    end
+    if curStep == 1189 then
+        shootthem(0.5)
+    end
+    if curStep == 1460 then
+        shootthem(0.5)
+    end
+    if curStep == 1476 then
+        shootthem(0.5)
+    end
+    if curStep == 1492 then
+        shootthem(0.5)
+    end
+    if curStep == 1581 then
+        shootthem(0.5)
+    end
+end
